3e63d22a5198037e687e06f2593a459cde36b9ae
dn <darknil@gmail.com>
2016-12-12 11:41:57 -0600

    Still unfinished

--- BattleScriptsTest/Program.cs
+++ BattleScriptsTest/Program.cs
@@ -9,63 +9,134 @@ namespace BattleScriptsTest
 {
     public class Program
     {
+        public int ScriptOffset;
+        public int 
 
         static void Main(string[] args)
         {
             RomFileIO Rom = new RomFileIO();
-            Rom.Open("./ff6mtest.sfc");
-            //ReadPointerNormal(Rom, BattleAIConstants.MONSTER_SCRIPT_POINTERS_NORMAL);
-            //ReadPointerHard(Rom, BattleAIConstants.MONSTER_SCRIPT_POINTERS_HARD);
+            Rom.Open("./FF6MTEST.sfc");
+            ExportScriptsNormal(Rom);
             Rom.Close();
         }
 
         static void ReadPointerNormal(RomFileIO Rom, int Offset)
         {
+            if (!Directory.Exists("./Normal/"))
+            {
+                Directory.CreateDirectory("./Normal/"); 
+            }
             int PointerLoop = 0;
             List<int> EnemyPointerNormal = new List<int>();
-            while (PointerLoop < 384*2)
+            while (PointerLoop < 512*2)
             {
                 int Pointer = Rom.Read16(Offset + PointerLoop);
                 EnemyPointerNormal.Add(Pointer);
                 PointerLoop++;
                 PointerLoop++;
             }
-            /*using (StreamWriter file = new StreamWriter("./pointertest.txt", false))
+            using (StreamWriter file = new StreamWriter("./Normal/normal_pointers.txt", true))
             {
                 int WriteLoop = 0;
-                while (WriteLoop < 384)
+                while (WriteLoop < 512)
                 {
                     file.WriteLine("{0:X}", EnemyPointerNormal[WriteLoop]);
                     WriteLoop++;
                 }
-            }*/
+            }
         }
 
         static void ReadPointerHard(RomFileIO Rom, int Offset)
         {
+            if (!Directory.Exists("./Hard/"))
+            {
+                Directory.CreateDirectory("./Hard/");
+            }
             int PointerLoop = 0;
             List<int> EnemyPointerHard = new List<int>();
-            while (PointerLoop < 384 * 2)
+            while (PointerLoop < 512 * 2)
             {
                 int Pointer = Rom.Read16(Offset + PointerLoop);
                 EnemyPointerHard.Add(Pointer);
                 PointerLoop++;
                 PointerLoop++;
             }
-            /*using (StreamWriter file = new StreamWriter("./pointertest.txt", true))
+            using (StreamWriter file = new StreamWriter("./Hard/hard_pointers.txt", true))
             {
                 int WriteLoop = 0;
-                while (WriteLoop < 384)
+                while (WriteLoop < 512)
                 {
                     file.WriteLine("{0:X}", EnemyPointerHard[WriteLoop]);
                     WriteLoop++;
                 }
-            }*/
+            }
         }
 
-        static void ReadScriptNormal(RomFileIO Rom, int Offset)
+        //*********************************************************************************************//
+
+        //This reads a single script and renders it as readable text
+        static void ReadSingleScriptNormal(RomFileIO Rom, int Offset)
+        {
+            if (!File.Exists("./FF6MTest.sfc"))
+            {
+                throw new FileNotFoundException("FF6MTest.sfc not found.");
+            }
+
+        }
+
+        //This reads a single script and renders it as bytecode
+        static void WriteSingleScriptNormal(RomFileIO Rom, int Offset)
+        {
+            //TODO: read and import a single script to a txt file in \Normal
+        }
+
+        //This reads a single script and renders it as readable text
+        static void ReadSingleScriptHard(RomFileIO Rom, int Offset)
+        {
+            //TODO: read and export a single script to a txt file in \Hard
+        }
+
+        //This reads a single script and renders it as bytecode
+        static void WriteSingleScriptHard(RomFileIO Rom, int Offset)
+        {
+            //TODO: read and import a single script to a txt file in \Hard
+        }
+
+        //*********************************************************************************************//
+
+        static void ReadScriptsNormal(RomFileIO Rom, int Offset)
+        {
+            //TODO: loop single script read 512 times
+        }
+
+        static void WriteScriptsNormal(RomFileIO Rom, int Offset)
+        {
+            //TODO: loop single script read 512 times
+        }
+
+        //*********************************************************************************************//
+
+        static void ExportScriptsNormal(RomFileIO Rom)
         {
             //Extract script and convert to readable format
+            ReadPointerNormal(Rom, BattleAIConstants.MONSTER_SCRIPT_POINTERS_NORMAL);
+            ReadScriptsNormal(Rom, BattleAIConstants.MONSTER_SCRIPTS_NORMAL);
+
+        }
+
+        static void ImportScriptsNormal(RomFileIO Rom, int Offset)
+        {
+            //Insert script and convert to byte format
+        }
+
+        static void ExportScriptsHard(RomFileIO Rom, int Offset)
+        {
+            //Extract script and convert to readable format
+        }
+
+        static void ImportScriptsHard(RomFileIO Rom, int Offset)
+        {
+            //Insert script and convert to byte format
         }
     }
 }
